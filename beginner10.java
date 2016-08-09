1830af4a109e0fcf7b3ad79db6cabaacbdd618ec
valliammaial <iamvalliammai@gmail.com>
2016-08-09 23:36:49 +0530

    Create beginner10.java

---
+++ beginner10.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class lb10
+{
+public static void main(String args[])
+{
+int n;
+int count=0;
+
+Scanner s=new Scanner(system.in);
+n=s.nextInt();
+while(n!=0)
+{
+count++;
+n=n/10;
+}
+system.out.println(count);
+}
+}
