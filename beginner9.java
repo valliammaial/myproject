5736b1a6949763ecdf09c3821ed35c4342ae42ba
valliammaial <iamvalliammai@gmail.com>
2016-08-09 23:30:58 +0530

    Create beginner9.java

---
+++ beginner9.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class lb9
+{
+public static void main(String args[])
+{
+int n;
+Scanner s=new Scanner(system.in);
+n=s.nextInt();
+int sum=0;
+for(int i=1;i<=n;i++)
+{
+sum+=i;
+}
+System.out.println(sum);
+}
+}
