cf58248840a3e948ef2c638edbf8f9fc514157d8
valliammaial <iamvalliammai@gmail.com>
2016-08-09 18:32:55 +0530

    Create beginner2.java

---
+++ beginner2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class lb2
+{
+public static void main(String args[])
+{
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+if(n%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
