70375a6dd7c785f948c75aef8ae1ead6a86ea771
valliammaial <iamvalliammai@gmail.com>
2016-08-09 23:11:48 +0530

    Create beginner7.java

---
+++ beginner7.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class lb6
+{
+public static void main(String args[])
+{
+String str;
+Scanner s=new Scanner(System.in);
+str=s.next();
+for(int i=0;i<5;i++)
+{
+System.out.println(str);
+}
+}
+}
