2e41925a187319f9aa9f6902bf115849ee6658fd
valliammaial <iamvalliammai@gmail.com>
2016-08-10 19:42:52 +0530

    Create player1.java

---
+++ player1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class p1
+{
+public static void main(String args[])
+{
+String str;
+Scanner s=new Scanner(System.in);
+str.s.next();
+StringBuffer ss=new StringBuffer(str);
+String s1=ss.reverse().toString();
+System.out.println(s1);
+}
+}
