80a31aeb270f81358bcbe038f0231740bb43be20
valliammaial <iamvalliammai@gmail.com>
2016-08-10 19:50:16 +0530

    Update player1.java

--- player1.java
+++ player1.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 {
 String str;
 Scanner s=new Scanner(System.in);
-str.s.next();
+str=s.next();
 StringBuffer ss=new StringBuffer(str);
 String s1=ss.reverse().toString();
 System.out.println(s1);
