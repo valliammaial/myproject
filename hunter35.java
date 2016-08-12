6748662dc452b920c97bceac0eb7df517c762df6
valliammaial <iamvalliammai@gmail.com>
2016-08-13 00:04:01 +0530

    Create hunter35.java

---
+++ hunter35.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class hunter35 {
+	public static void main(String[] args) {
+
+String str;
+Scanner s=new Scanner(System.in);
+System.out.println("enter string");
+str=s.next();
+int len=str.length();
+int half=len/2;
+String s1=str.substring(0,half);
+System.out.println(s1);
+String s2=str.substring(half);
+System.out.println(s2);
+if(s1.equals(s2))
+	System.out.println("double strings");
+else
+	System.out.println("not double strings");
+	}
+
+}
