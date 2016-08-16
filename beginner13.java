3c3491d0aa2f488b861dd91b8795326c7676c84f
valliammaial <iamvalliammai@gmail.com>
2016-08-16 17:57:16 +0530

    Update beginner13.java

--- beginner13.java
+++ beginner13.java
@@ -7,18 +7,19 @@ public static void main(String args[])
 {
 int n;
 Scanner s=new Scanner(System.in);
-System.out.println("Enter n value");
+
 n=s.nextInt();
+int count=0;
 for(int i=2;i<n;i++)
 {
 if(n%2==0)
 {
 System.out.println("The given no is not prime");
+count++;
 break;
 }
-else
-{
-System.out.println("The given no is prime");
 }
+if(count==0)
+System.out.println("The given no is prime");
 }
 }
