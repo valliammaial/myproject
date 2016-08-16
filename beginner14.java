1e9271f30816706ca0cdaf2ea2a6c5906020802e
valliammaial <iamvalliammai@gmail.com>
2016-08-16 18:06:15 +0530

    Create beginner14.java

---
+++ beginner14.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class b14
+{
+public static void main(String args[])
+{
+int inter1;
+int inter2;
+for(int i=inter1;i<=inter2;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+}
+}
+}
