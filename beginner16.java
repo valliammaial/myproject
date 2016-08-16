69c09fd9b48fa8a28d7f5334f59fa8011a1f353c
valliammaial <iamvalliammai@gmail.com>
2016-08-16 18:34:18 +0530

    Create beginner16.java

---
+++ beginner16.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class b16
+{
+public static void main(String args[])
+{
+int inter1;
+int inter2;
+Scanner s=new Scanner(System.in);
+inter1=s.nextInt();
+inter2=s.nextInt();
+boolean flag=false;
+for(int i=inter1;i<=inter2;i++)
+{
+flag=false;
+for(int j=2;j<i;j++)
+{
+if(i%j==0)
+{
+flag=true;
+break;
+}
+else
+{
+continue;
+}
+}
+if(!flag)
+{
+system.out.println(i);
+}
+}
+}
+}
