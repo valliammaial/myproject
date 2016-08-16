da72d2dcad9a366662a617561a1e73a9bd7bdca6
valliammaial <iamvalliammai@gmail.com>
2016-08-16 18:07:28 +0530

    Update beginner14.java

--- beginner14.java
+++ beginner14.java
@@ -7,6 +7,9 @@ public static void main(String args[])
 {
 int inter1;
 int inter2;
+Scanner s=new Scanner(System.in);
+inter1=s.nextInt();
+inter2=s.nextInt();
 for(int i=inter1;i<=inter2;i++)
 {
 if(i%2!=0)
