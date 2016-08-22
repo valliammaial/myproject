6d53feb7c1b41472f161a430f04df806ee3f6ace
valliammaial <iamvalliammai@gmail.com>
2016-08-22 18:14:09 +0530

    Create hunter11.java

---
+++ hunter11.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class h11
+{
+public static void main(String args[])
+{
+String str;
+Scanner s=new Scanner(System.in);
+str=s.nextLine();
+String ss[]=str.split(" ");
+for(int i=ss.length;i>=0;i--)
+System.out.println(ss[i]);
+}
+}
