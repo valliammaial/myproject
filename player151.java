b075ca4b7d58e5405dc5972835785896ad8846e6
valliammaial <iamvalliammai@gmail.com>
2016-08-25 12:19:01 +0530

    Create player151.java

---
+++ player151.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class p151
+{
+public static void main(String args[])
+{
+for(int i=0;i<4;i++)
+{
+for(int j=0;j<i+1;j++)
+{
+System.out.print("#");
+}
+System.out.print(" ");
+}
+}
+}
