73e10815546cca9df327917e6109d9f6f45edcdc
valliammaial <iamvalliammai@gmail.com>
2016-09-02 11:53:58 +0530

    Create hunter6.java

---
+++ hunter6.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class h6
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+Map<Integer,Integer> m=new LinkedHashMap<Integer,Integer>();
+for(int i=0;i<n;i++)
+{
+m.put(a[i],(m.get(a[i])==null)?1:m.get(a[i])+1);
+}
+List<Integer> list=new ArrayList<Integer>(m.keySet());
+for(int i:list)
+{
+if(m.get(a[i])>1)
+{
+System.out.println(a[i]);
+break;
+}
+}
+}
+}
